ae88b4ddd20c42639335401e1fb6c529439be104
widhisec <39062772+widhisec@users.noreply.github.com>
2023-08-12 23:06:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ NAME_="TESTED"
 OKE_=$(pwd)
 # created by widhisec
 zip -r $NAME_ *
-unzip $NAME_ -d "${OKE}/oke"
+unzip $NAME_.apk -d "$NAME_"
 mv "$NAME_.zip" "${NAME_}.apk" 
 keytool -genkey -v -keystore my-release-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=com.telkomsel.roli, OU=ID, O=APK, L=Unknown, S=Unknown, C=XK" -storepass "${DEFAULT_}"
 apksigner sign -ks my-release-key.keystore --ks-key-alias my-key-alias --ks-pass "pass:${DEFAULT_}" ${NAME_}.apk 
