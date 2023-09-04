6386e1311439889ccd06c3bd8d5ec9f012f2c0aa
widhisec <39062772+widhisec@users.noreply.github.com>
2023-09-04 16:59:27 +0700

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 DEFAULT_="44444444"
-NAME_="TESTED"
+ NAME_="oke.apk"
 OKE_=$(pwd)
 # created by widhisec
-zip -r $NAME_ *
-unzip $NAME_.apk -d "$NAME_"
-mv "$NAME_.zip" "${NAME_}.apk" 
-keytool -genkey -v -keystore my-release-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=com.telkomsel.roli, OU=ID, O=APK, L=Unknown, S=Unknown, C=XK" -storepass "${DEFAULT_}"
-apksigner sign -ks my-release-key.keystore --ks-key-alias my-key-alias --ks-pass "pass:${DEFAULT_}" ${NAME_}.apk 
-#> signed.apk
-zipalign -p -f 4 $NAME_.apk aligned.apk
+# unzip ${NAME_}  -d ${OKE_}/oke
+# zip -r $NAME_ *
+# # python3  << BR 
+# # import lief
+# # native = lief.parse(libpath)
+# # native.add_library("libgadget.so")
+# # native.write()
+# # BR
+#  mv oke.zip oke.apk
+#  keytool -genkey -v -keystore my-release-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=com.telkomsel.roli, OU=ID, O=APK, L=Unknown, S=Unknown, C=XK" -storepass "${DEFAULT_}"
+#  apksigner sign -ks my-release-key.keystore --ks-key-alias my-key-alias --ks-pass "pass:${DEFAULT_}" ${NAME_}
+# # #> signed.apk
+#  zipalign -p -f 4 ${NAME_} aligned.apk
